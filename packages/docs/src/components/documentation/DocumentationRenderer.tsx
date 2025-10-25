a06bd16480ff137c5908e70a4d1dcbc2a4aa6b80
Darshan-Naik <darshu8314@gmail.com>
2025-10-25 17:47:04 +0530

    updated doc app

--- packages/docs/src/components/documentation/DocumentationRenderer.tsx
+++ packages/docs/src/components/documentation/DocumentationRenderer.tsx
@@ -23,7 +23,7 @@ export function DocumentationRenderer({ data }: DocumentationRendererProps) {
     return (
         <div className="max-w-4xl mx-auto">
             {/* Header */}
-            <div className="bg-gradient-to-r from-blue-50 to-purple-50 rounded-xl p-6 border border-blue-100">
+            <div className="bg-gradient-to-r from-blue-50 to-purple-50 rounded-xl p-6 border border-blue-100 my-6">
                 <div className="flex items-center mb-3">
                     <div className="w-10 h-10 bg-gradient-to-r from-primary-600 to-secondary-600 rounded-lg flex items-center justify-center mr-4">
                         <BrainCircuit className="w-6 h-6 text-white" />
