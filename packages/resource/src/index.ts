b888aba8192c4c9606dfa326c18dc12b108f004c
Darshan-Naik <darshu8314@gmail.com>
2026-07-18 14:34:32 +0530

    refactor: remove internal utility exports from public API

--- packages/resource/src/index.ts
+++ packages/resource/src/index.ts
@@ -1,16 +1,3 @@
 export * from "./types";
 export { createResource } from "./resource";
 export { createCollection } from "./collection";
-export {
-    getByPath,
-    setByPath,
-    deleteByPath,
-    hasPath,
-    diffPaths,
-    applyOverrides,
-} from "./path";
-export {
-    flattenFieldsConfig,
-    isEditable,
-    computeFieldState,
-} from "./field";
